d0fa081e8b6a2688fe254526b40596627826e843
Plbyatt86 <byatt.pete@gmail.com>
2024-03-31 23:23:07 -0400

    original commit

---
+++ test/polybius.test.js
@@ -0,0 +1,22 @@
+// Write your tests here!
+const { expect } = require("chai");
+const { polybius } = require("../src/polybius");
+
+describe("polybius", () => {
+  // Write your tests here!
+  it("should encode a message properly", () => {
+    // Test for encoding a message
+  });
+
+  it("should decode a message properly", () => {
+    // Test for decoding a message
+  });
+
+  it("should translate 42 to both 'i' and 'j'", () => {
+    // Test for handling 42 as both 'i' and 'j'
+  });
+
+  it("should leave spaces as is", () => {
+    // Test for leaving spaces unchanged
+  });
+});
---
+++ test/substitution.test.js
@@ -0,0 +1,21 @@
+// Write your tests here!
+const { expect } = require("chai");
+const { substitution } = require("../src/substitution");
+
+describe("substitution", () => {
+  it("should encode a message properly", () => {
+    // Test for encoding a message
+  });
+
+  it("should decode a message properly", () => {
+    // Test for decoding a message
+  });
+
+  it("should handle unique substitution alphabet", () => {
+    // Test to make sure substitution alphabet is unique
+  });
+
+  it("should return false if substitution alphabet is not exactly 26 characters", () => {
+    // Test to make sure the substitution alphabet is exactly 26 characters
+  });
+});
